1ca10f10b9f7e63806dd81456494005015507b2e
Ghayth Sabeaallil <gs222mh@gmail.com>
2024-05-29 10:23:39 +0200

    add copy btn

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,41 +1,49 @@
 <script setup lang="ts">
-defineProps<{ fname: string, lname: string, email: string, avatar: string }>()
+defineProps<{ fname: string, lname: string, email: string, avatar: string }>();
+function copy(email : string){
+    navigator.clipboard.writeText(email);
+}
 </script>
 
 <template>
     <div class="card-container">
         <img class="avatar" :src=avatar :alt=fname>
         <h2>{{ fname }} {{ lname }}</h2>
-        <a :href="'mailto:' + email"><img class="mail" src="/mail.png" alt="mail"></a>
-            </div>
-
+        <div> <a :href="'mailto:' + email"><img src="/mail.png" alt="mail"></a>
+            <img @click="copy(email)" src="/copy.png" alt="copy email">
+        </div>
+    </div>
 </template>
 
 <style scoped>
-.card-container{
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-max-width: 500px;
-gap:1em;
+.card-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 500px;
+    gap: 1em;
 }
-.avatar{
+
+.avatar {
     border-radius: 50%;
     width: 200px;
     height: 200px;
 }
-.avatar:hover{
+
+.avatar:hover {
     border: 5px solid salmon;
     -webkit-box-shadow: 0 0 20px #000000;
-        box-shadow: 0 0 20px #000000;
-}
-.mail{
-    width: 35px;
-    height: 35px;
+    box-shadow: 0 0 20px #000000;
 }
 
-h2{
+img {
+    width: 35px;
+    height: 35px;
+    cursor: pointer;
+}
+
+h2 {
     color: rgb(108, 110, 110);
 
 }
